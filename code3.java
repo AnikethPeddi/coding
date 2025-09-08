0a48ad5c5ef1e50d549c1b195d3d177d437dc154
Aniketh <158760450+AnikethPeddi@users.noreply.github.com>
2025-09-08 22:42:18 +0530

    Create code3.java
    Ask the user to enter three numbers: n1, n2, and n3.
    
    If the numbers are in increasing order, print: "Increasing Order"
    
    If the numbers are in decreasing order, print: "Decreasing Order"
    
    Otherwise, print: "Neither increasing nor decreasing"

---
+++ code3.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+        int n3 = scan.nextInt();
+        System.out.println("Enter 3 numbers");
+
+        if (n1 < n2 && n2 < n3) {
+            //if(n2<n3){
+            System.out.println("Increasing Order");
+            //}
+        } else if (n1 > n2 && n2 > n3) {
+            //               if(n2>n3){
+            System.out.println("Decreasing Order");
+        } else {
+
+            System.out.println("Either Increasing or Decreasing");
+
+        }
+
+    }
+}
